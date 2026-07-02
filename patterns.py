32e62e162c7eebfc7d0f3a888673d00ee1b41c03
keerthika <keerthii0407@gmail.com>
2026-07-02 17:58:16 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,43 @@
+#to print right angle triangle * pattern
+'''m=6
+for i in range(0,m):
+    for j in range(0,i+1):
+        print('*',end=" ")
+    print()'''
+#to print numbers in patterns method
+'''n=5
+num=1
+for i in range(0,n):
+    for j in range(0,i+1):
+        print(num,end=' ')
+        num+=1
+    print()'''
+# to print odd numbers in pattern method
+'''n=5
+num=1
+for i in range(0,n):
+    for j in range(0,i+1):
+        print(num,end=' ')
+    num+=2
+    print()'''
+# to print pattern ..............h/w
+'''n=4
+for i in range(0,n):
+    for j in range(0,i+1):
+        print('2',end=' ')
+    print()'''
+#to print square pattern
+'''size=4
+for i in range (size):
+    for j in range(size):
+        print('*',end=' ')
+    print()'''
+#to print abcd pattern
+'''num=65
+n=5
+for i in range(0,n):
+    for j in range(0,i+1):
+        ch=chr(num)
+        print(ch,end=' ')
+    num=+1
+    print()'''
